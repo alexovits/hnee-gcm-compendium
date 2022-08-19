4f424c725fc8a9a802333e220bdbe3574799adb2
Zsolt Szabo <zsoltszabo@Zsolts-MacBook-Air.local>
2022-08-19 12:39:16 +0300

    Add direct linked styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,10 @@ export default function Home() {
         <title>Compendium</title>
         <meta name="description" content="Compendium" />
         <link rel="icon" href="/hnee-green.png" />
+        <link
+          href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,300,400,700"
+          rel="stylesheet"
+        />
       </Head>
 
       <main className={styles.main}>
