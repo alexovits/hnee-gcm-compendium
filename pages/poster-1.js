4e1a0eafd88cd62b3df5e12a05a32c57d6dc1d26
Zsolt Szabo <zsoltszabo@Zsolts-MacBook-Air.local>
2022-08-19 18:07:01 +0300

    Mr worldwide added

---
+++ pages/poster-1.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { MapInteractionCSS } from 'react-map-interaction';
+
+export default function Home() {
+  return (
+    <div className={styles.mainImageZoomContainer}>
+      <Head>
+        <title>Compendium</title>
+        <meta name="description" content="Compendium" />
+        <link rel="icon" href="../public/hnee-green.png" />
+      </Head>
+
+      <div className={styles.articleHeader}>
+        <a href="/">
+          <img src="/hnee.png" href="/sdfsd" alt="hnee-logo" width="auto" id={styles.hneeArticleHeader}/>
+        </a>
+      </div>
+      <div className={styles.mapContainer}>
+      <MapInteractionCSS>
+        <img src="poster/worldwide.jpg" width={'auto'} className={styles.zoomImage}/>
+      </MapInteractionCSS>
+      </div>
+
+    </div>
+  )
+}
