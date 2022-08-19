918e455d163d02fcafdfc51702d8f5fef4896f59
Zsolt Szabo <zsoltszabo@Zsolts-MacBook-Air.local>
2022-08-19 12:03:09 +0300

    Dont optimize fonts

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  optimizeFonts: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
