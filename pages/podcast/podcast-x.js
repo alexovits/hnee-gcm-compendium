ae93778f638d996bae54d9c41d9e5b03f3785535
Zsolt Szabo <debeloksanyi@gmail.com>
2022-08-29 17:23:58 +0200

    fix

--- pages/podcast/podcast-x.js
+++ pages/podcast/podcast-x.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '/styles/Home.module.css'
 import { MapInteractionCSS } from 'react-map-interaction';
-import YoutubeEmbed from "./YoutubeEmbed";
+import YoutubeEmbed from "../YoutubeEmbed";
 
 export default function Podcast() {
   return (
